3fc98783db5d6aeb165f0dd6586c6fec6d404bd3
chatRG <>
2018-02-16 00:40:01 +0530

    Added store for constants

---
+++ app/src/main/java/com/wallapp/store/CustomConstants.java
@@ -0,0 +1,24 @@
+package com.wallapp.store;
+
+public interface CustomConstants {
+    String ALPHA_BASE_URL = "https://source.unsplash.com/featured/";
+    String BING_BASE_URL = "https://bing.com/";
+    String BING_DAILY_URL = BING_BASE_URL + "HPImageArchive.aspx?format=js&n=7&idx=0";
+
+    String PREF_FIRST_START = "FIRST_START";
+    String PREF_SET_AS = "SET_AS";
+
+    String SRC_ALPHA = "Alpha";
+    String SRC_BING = "Bing daily";
+
+    String DATE_FORMAT = "yyyyMMdd_HHmmss";
+
+    int HDWidth = 1280;
+    int HDHeight = 720;
+    int FHDWidth = 1920;
+    int FHDHeight = 1080;
+    int MAXWidth = 2880;
+    int MAXHeight = 1680;
+
+    String APP_NAME = "WallApp";
+}
